d5d508a3f9664dbbc15d59b56be83ed868856d1c
Lane Le Prevost-Smith <lanelps@gmail.com>
2022-01-03 17:08:24 +1300

    webpack css and fonts

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log(`Hello!`);
+import './styles/main.css';
+
+function initialiseBoard() {
+  const element = document.createElement('div');
+  element.innerHTML = `:) :(`;
+
+  document.body.appendChild(element);
+}
+
+function main() {
+  if (typeof window !== 'undefined') {
+    window.addEventListener('DOMContentLoaded', () => {
+      console.log(`Hello!`);
+      initialiseBoard();
+    });
+  }
+}
+
+main();
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,22 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  mode: process.env.NODE_ENV !== 'production' ? 'development' : 'production',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: '[name][ext]'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource'
+      }
+    ]
   }
 };
