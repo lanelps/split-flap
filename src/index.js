04ff15d8151b9d6e9eef72a93e67ae59adeb2d00
Lane Le Prevost-Smith <lanelps@gmail.com>
2022-01-10 16:11:44 +1300

    simple split flap text

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import './styles/main.css';
 
-const PIXEL_WIDTH = 9.63;
-const PIXEL_HEIGHT = 16;
-
 function colorShuffle(pixel) {
   const max = 255;
   const r = Math.floor(Math.random() * max);
@@ -11,16 +8,32 @@ function colorShuffle(pixel) {
   pixel.style.color = `rgb(${r}, ${g}, ${b})`;
 }
 
-function textShuffle(pixel) {
-  const characters = `1234567890-=~!@#$%^&*()_+,./<>?≈ç√∫˜µ≤≥÷åß∂ƒ©˙∆˚¬…æœ∑´®†¥¨ˆøπ“‘«[]\{}|`;
+function splitFlap(element) {
+  const initialText = element.innerText;
 
+  const characters = `FuckYou`;
   const charArray = characters.split(``);
-  pixel.innerText = charArray[Math.floor(Math.random() * charArray.length)];
+
+  let loopCounter = 0;
+
+  const sliptLoop = () => {
+    const timeout = setTimeout(() => {
+      if (loopCounter < charArray.length) {
+        element.innerText = charArray[loopCounter];
+        loopCounter++;
+        sliptLoop();
+      } else {
+        element.innerText = initialText;
+        clearTimeout(timeout);
+      }
+    }, 100);
+  };
+
+  sliptLoop();
 }
 
-function splitFlap(pixel) {
-  const initialText = pixel.innerText;
-  const initialColor = pixel.style.color;
+function splitFlapRandom(element) {
+  const initialText = element.innerText;
 
   const characters = `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz`;
   const charArray = characters.split(``);
@@ -30,14 +43,12 @@ function splitFlap(pixel) {
   const sliptLoop = () => {
     const timeout = setTimeout(() => {
       if (loopCounter < 10) {
-        pixel.innerText =
+        element.innerText =
           charArray[Math.floor(Math.random() * charArray.length)];
         loopCounter++;
-        pixel.style.color = colorShuffle(pixel);
         sliptLoop();
       } else {
-        pixel.innerText = initialText;
-        pixel.style.color = initialColor;
+        element.innerText = initialText;
         clearTimeout(timeout);
       }
     }, 100);
@@ -46,73 +57,45 @@ function splitFlap(pixel) {
   sliptLoop();
 }
 
-function redrawBoard() {
-  const board = document.querySelector(`#board`);
+function createSplitFlapElement(type, text) {
+  const element = document.createElement(type);
 
-  while (board.firstChild) {
-    board.removeChild(board.firstChild);
-  }
+  const textChars = text.split('');
+  textChars.forEach(char => {
+    const charElement = document.createElement(`span`);
+    charElement.innerText = char;
 
-  drawGrid();
-}
+    charElement.addEventListener(`mouseover`, () => {
+      splitFlapRandom(charElement);
+      // splitFlap(charElement);
+    });
 
-function windowResize() {
-  const resizeObserver = new ResizeObserver(entries => {
-    for (let entry of entries) {
-      redrawBoard();
-    }
+    element.appendChild(charElement);
   });
 
-  resizeObserver.observe(document.querySelector(`body`));
+  return element;
 }
 
 function eventListeners() {}
 
-function drawGrid() {
-  const boardWidth = Math.floor(board.clientWidth / PIXEL_WIDTH);
-  const boardHeight = Math.floor(board.clientHeight / PIXEL_HEIGHT);
+function init() {
+  const heading = createSplitFlapElement(`h1`, `HELLO;`);
+  const sentence = createSplitFlapElement(
+    `p`,
+    `Tēnā koutou, tēnā koutou, tēnā koutou katoa`
+  );
 
-  for (let i = 0; i < boardHeight; i++) {
-    for (let j = 0; j < boardWidth; j++) {
-      const pixel = document.createElement(`span`);
-      pixel.setAttribute('class', 'pixel');
-      pixel.style.cssText = `
-      left: ${j * PIXEL_WIDTH}px;
-      top: ${i * PIXEL_HEIGHT}px;
-      width: ${PIXEL_WIDTH}px;
-      height: ${PIXEL_HEIGHT}px;
-      color: #000;
-      `;
-      pixel.innerText = `*`;
-      // textShuffle(pixel);
-
-      pixel.addEventListener('mouseover', () => {
-        splitFlap(pixel);
-      });
-
-      board.appendChild(pixel);
-    }
-  }
-}
-
-function initialiseBoard() {
-  const board = document.createElement('div');
-  board.setAttribute('id', 'board');
-  document.body.appendChild(board);
-
-  drawGrid();
+  document.body.appendChild(heading);
+  document.body.appendChild(sentence);
 }
 
 function main() {
   if (typeof window !== 'undefined') {
     window.addEventListener('DOMContentLoaded', () => {
-      windowResize();
+      init();
       eventListeners();
-      initialiseBoard();
     });
   }
 }
 
 main();
-
-// if the body is moused down then and individual pixel mouseover function should be listening
